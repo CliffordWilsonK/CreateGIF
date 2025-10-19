c9c483118e1f462c8cd52dc0bafaac8b9ef0d81e
wilsoncliffordkwame-oss <wilsoncliffordkwame>
2025-10-19 19:36:39 +0000

    Version1 of the program built

--- create_gif.py
+++ create_gif.py
@@ -1,2 +1,8 @@
 import imageio.v3 as iio
 
+filenames = ['images/img_1.png', 'images/img_2.png']
+images = []
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('images/team.gif', images,  duration=500, loop=0)
